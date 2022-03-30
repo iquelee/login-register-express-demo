94430cd8940bb5f21bff01d2dfe93ef81e7b25f0
iquelee <iquelee@gmail.com>
2022-03-30 17:38:54 +0800

    master

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 // 引入依赖
-const express = require("express")
+const express = require('express')
+const path = require('path')
+const fs = require('fs')
+
+const user = require('./routes/user')
 
 // 创建服务器
 const app = express()
@@ -7,10 +11,16 @@ const app = express()
 // 添加ejs模板引擎
 app.set('view engine', 'ejs')
 
-// 监听测试路径
-app.get("/", (req, res) => {
-  res.send("yo.")
-})
+//解决跨域问题
+app.use(require('cors')())
+
+// POST参数接受中间件
+app.use( express.urlencoded({extended: false}))
+
+// 加载用户模块
+app.use('/user', user)
 
 // 启动服务器
-app.listen(3000, () => console.log("服务器已经启动...."))
+app.listen(3000, () => {
+  console.log('http://localhost:3000')
+})
--- routes/user.js
+++ routes/user.js
@@ -1,21 +1,64 @@
+// 引入依赖
 const express = require("express")
+const fs = require('fs')
+const path = require('path')
+
+// 初始化数据
 const router = express.Router()
 
-router.get("/register", (req, res) => {
-  // 返回register.ejs页面，返回提示消息[200表示成功，msg需要展示的错误信息]
-  res.render('register', {code: 200, msg: ""})
+// 声明变量
+let userArray = ''
+
+router.get('/register', (req, res) => {
+  res.send('等待注册')
 })
 
-router.post("/register", (req, res) => {
+router.post('/register', (req, res) => {
   // 获取用户参数
   let {username,
       password,
       nickname,
       headerImg= '/public/uploads/default.jpg'} = req.body
-  console.log(username, password, nickname, headerImg)
-  // TODO 注册过程
-  res.render('register', {code: 200, msg: "用户正在注册"})
+  // 验证账号是否存在，存在直接报错
+  for (let user of userArray) {
+    if (user.username === username ) {
+      res.render('', { code: 501, msg: "账号已经被占用" })
+      return
+    }
+  }
+  // 创建一个新用户对象
+  let user = {username, password, nickname, headerImg}
+  // 保存到系统中，并提示注册成功
+  userArray.push(user)
+  saveData()
+  res.render('', { code: 200, msg: "注册成功" })
 })
+
+// 读取系统文件
+function initData() {
+  // 读取系统中的users.json文件
+  fs.readFile(path.join(__dirname, '../users.json'), 'utf-8', (err, data) => {
+    if (err) {
+      // 如果出现错误，表示文件没有读取到；创建文件
+      fs.writeFile(path.join(__dirname, '../users.json'), '[]', () => {
+        console.log('文件创建成功')
+      })
+    } else {
+      // 如果文件存在，直接写入系统变量内容，将读取到的文件内容转换成对象赋值
+      userArray = JSON.parse(data)
+    }
+  })
+}
+
+// 存储数据到文件的函数，在项目中修改了数据时再去调用
+function saveData() {
+  fs.writeFile(path.join(__dirname, '../users.json'), JSON.stringify(userArray), (err) => {
+    console.log('系统数据存储完成')
+  })
+}
+
+// 调用执行函数
+initData()
 
 module.exports = router
 
